029a809dde991e6ba74b0523a6910403a2392550
sisiira <sisiiraa.m@gmail.com>
2024-03-06 10:31:16 +0100

    feat(loadingpage): various improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,9 +27,7 @@ export default function Home() {
                 <main>
                     <section id="Bienvenue" className={styles.container}>
                         <h3>Hemfree</h3>
-                        <p>
-                            Les meilleurs serveurs Minecraft
-                        </p>
+                        <p>Où construire prend un autre goût</p>
                     </section>
                     <section id="Serveurs" className={styles.servers}>
                         <h3 className={styles.heading}>Nos serveurs</h3>
@@ -52,50 +50,35 @@ export default function Home() {
                             styles.contentSection,
                         ].join(' ')}
                     >
-                        <div className="CON">
-                            <div className="row">
-                                <div className="CC">
-                                    <h2>A propos</h2>
-                                    <p>Vous pouvez nous consultez sur</p>
-                                    <ul className="list-inline banner-social-buttons"></ul>
-                                    <li className="list-inline-item">
-                                        <button
-                                            className="btn btn-primary btn-lg btn-default"
-                                            type="button"
-                                        >
-                                            <i className="fa fa-facebook-plus fa-fw"></i>
-                                            <span className="network-name">
-                                                <a
-                                                    href="https://www.facebook.com/profile.php?id=61556567765376"
-                                                    target="_blank"
-                                                >
-                                                    Facebook
-                                                </a>
-                                            </span>
-                                        </button>
-                                    </li>
-                                    <h1></h1>
-                                    <li className="list-inline-item">
-                                        <button
-                                            className="btn btn-primary btn-lg btn-default"
-                                            type="button"
-                                        >
-                                            <i className="fa fa-twitter fa-fw"></i>
-                                            <span className="network-name">
-                                                <a
-                                                    href="https://twitter.com/HemfreeCraft"
-                                                    target="_blank"
-                                                >
-                                                    Twitter
-                                                </a>
-                                            </span>
-                                        </button>
-                                    </li>
-                                </div>
-                            </div>
-                        </div>
+                        <div className="aboutSection">
+                          <h2>A propos</h2>
+                           <p>Vous pouvez nous consulter sur</p>
+                             <ul className="list-inline banner-social-buttons">
+                               <li className="list-inline-item">
+                                 <button className="btn facebook" type="button">
+                                   <i className="fa fa-facebook"></i>
+                                     <span className="network-name">
+                                       <a href="https://www.facebook.com/profile.php?id=61556567765376" target="_blank">
+                                          Facebook
+                                        </a>
+                                     </span>
+                                 </button>
+                             </li>
+                       <li className="list-inline-item">
+                           <button className="btn twitter" type="button">
+                             <i className="fa fa-twitter"></i>
+                             <span className="network-name">
+                                 <a href="https://twitter.com/HemfreeCraft" target="_blank">
+                                 Twitter
+                                 </a>
+                             </span>
+                           </button>
+                         </li>
+                       </ul>
+                     </div>
+
                     </section>
-                </main>
+                    </main>
                 <footer>
                     <h2>Contact us</h2>
                     <a href="https://myaccount.google.com/u/5/?hl=fr&utm_source=OGB&utm_medium=act&pli=1">
