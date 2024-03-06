92a817620976cc0356706e32dc1f378afc20ae6f
Mathys Visticot <mathys.vst@gmail.com>
2024-03-06 14:49:14 +0100

    fix(cssHtml): fix more stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,57 +3,20 @@ import styles from './page.module.css';
 export default function Home() {
     return (
         <main>
-            <section id="Bienvenue" className={styles.container}>
-                <h3>Hemfree</h3>
-                <p>Où construire prend un autre goût</p>
+            <section className={styles.titleContainer}>
+                <h2>Hemfree</h2>
+                <h3>Où construire prend un autre goût</h3>
             </section>
-            <section id="Serveurs" className={styles.servers}>
-                <h3 className={styles.heading}>Nos serveurs</h3>
-                <p className={styles.subHeading}>Choisissez votre serveur</p>
-                <div className={styles.serverContainer}>
-                    <a href="Serveur2.html" className={styles.server}>
+            <section className={styles.serversContainer}>
+                <h3>Nos serveurs</h3>
+                <p>Choisissez votre serveur</p>
+                <div>
+                    <a href="/servers/1" className={styles.serversContainer}>
                         Serveur 1
                     </a>
-                    <a href="Serveur1.html" className={styles.server}>
+                    <a href="/servers/2" className={styles.serversContainer}>
                         Serveur 2
                     </a>
-                </div>
-            </section>
-            <section
-                id="Contact"
-                className={[styles.textCenter, styles.contentSection].join(' ')}
-            >
-                <div className="aboutSection">
-                    <h2>A propos</h2>
-                    <p>Vous pouvez nous consulter sur</p>
-                    <ul className="list-inline banner-social-buttons">
-                        <li className="list-inline-item">
-                            <button className="btn facebook" type="button">
-                                <i className="fa fa-facebook"></i>
-                                <span className="network-name">
-                                    <a
-                                        href="https://www.facebook.com/profile.php?id=61556567765376"
-                                        target="_blank"
-                                    >
-                                        Facebook
-                                    </a>
-                                </span>
-                            </button>
-                        </li>
-                        <li className="list-inline-item">
-                            <button className="btn twitter" type="button">
-                                <i className="fa fa-twitter"></i>
-                                <span className="network-name">
-                                    <a
-                                        href="https://twitter.com/HemfreeCraft"
-                                        target="_blank"
-                                    >
-                                        Twitter
-                                    </a>
-                                </span>
-                            </button>
-                        </li>
-                    </ul>
                 </div>
             </section>
         </main>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { Fragment } from 'react';
 
 import styles from './footer.module.css';
@@ -9,10 +10,30 @@ export const Footer = ({ children }: { children: React.ReactNode }) => {
             <footer className={styles.footer}>
                 <div className={styles.itemsContainer}>
                     <h2>Contact us</h2>
+                    <p>Vous pouvez nous retrouver sur</p>
+                    <div className={styles.socialContainer}>
+                        <button type="button">
+                            <Link href="https://www.facebook.com/profile.php?id=61556567765376">
+                                Facebook
+                            </Link>
+                        </button>
+                        <button type="button">
+                            <Link
+                                href="https://twitter.com/users
+                            "
+                            >
+                                Twitter
+                            </Link>
+                        </button>
+                        <button type="button">
+                            <Link href="https://www.instagram.com/hemfree/">
+                                Instagram
+                            </Link>
+                        </button>
+                    </div>
                     <a href="https://myaccount.google.com/u/5/?hl=fr&utm_source=OGB&utm_medium=act&pli=1">
                         Hemfree.minecraft@gmail.com
                     </a>
-                    <a href="link">123-456-7890</a>
                     <p>&copy; 2024 - Hemfree</p>
                 </div>
             </footer>
